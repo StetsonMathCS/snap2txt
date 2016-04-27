8e8d75549cec0ccd5dc1cddc5ae4983eb4349ce7
Caitlin <caitlin@the-prestridges.com>
2016-04-27 13:44:16 -0400

    Bind

--- db/database.cpp
+++ db/database.cpp
@@ -29,18 +29,37 @@ Database::Database(const char *dbfile){
 }
 
 void Database::insert(const char *fName, const char *eText, const  char *fDate){
-	int retval;
-	char* errmsg;
+        int retval;
+        char* errmsg;
+        sqlite3_stmt *s;
+        const char *test;
+        char *query;
 
-	retval = sqlite3_exec(db, "INSERT OR REPLACE INTO snaps VALUES (\"cats\", \"hello\", \"April 15, 2016\"), (\"dogs\", \"GoodBye\", \"May 9, 2016\");", NULL, NULL, &errmsg);
-	
-		
-	if(retval != SQLITE_OK)
-	{
-		cout << "Error in previous command: " << errmsg << endl;
-		sqlite3_free(errmsg);
-	}
+        //Insert data item into snaps
+        query = "INSERT INTO snaps (filename, exText, date) values (?,?,?)";
+
+        retval = sqlite3_prepare(db, query, strlen(query), &s, &test);
+
+        if (retval == SQLITE_OK) {
+                //Bind text
+                sqlite3_bind_text(s, 1, fName, strlen(fName), 0);
+                sqlite3_bind_text(s, 2, eText, strlen(eText), 0);
+                sqlite3_bind_text(s, 3, fDate, strlen(fDate), 0);
+        }
+
+        if(retval != SQLITE_OK)
+        {
+                cout << "Error in previous command: " << errmsg << endl;
+                sqlite3_free(errmsg);
+        }
+
+        retval = sqlite3_exec(db, query, NULL, NULL, &errmsg);
+
+        //Commit
+        sqlite3_step(s);
+        sqlite3_finalize(s);
 }
+
 
 void Database::close(){
 	sqlite3_close(db);
